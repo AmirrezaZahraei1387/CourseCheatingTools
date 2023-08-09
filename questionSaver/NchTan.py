4bbaa006828e3113dacf19901d560fe24e17e44d
amirrezazahraei <amirreza.zahraei1@rbe.sk.ca>
2023-08-09 17:32:44 -0600

    two access functions for getting access to the __title and __choices are created in class NChoiceTAnswer. also two other functions for searching among choices and setting the accuracy of a choice is created.

--- questionSaver/NchTan.py
+++ questionSaver/NchTan.py
@@ -7,7 +7,7 @@ class NChoiceTAnswer:
     __title: str = ""
     __choices: list = []
 
-    def __init__(self, title="", choices=None):
+    def __init__(self, title, choices):
         """title is the title of the question and the choices are the choices.
         for example in the following example:
 
@@ -20,9 +20,31 @@ class NChoiceTAnswer:
         what is an apple is title and the rest is choices."""
 
         self.__title = title
+        self.__choices = choices
 
-        if choices is not None:
-            self.__choices = choices
+    @property
+    def title(self):
+        return self.__title
+
+    def getChoice(self, index):
+        return self.__choices[index]
+
+    def setChoice(self, index):
+        """this function will set the mode of a choice.
+        true if it is true and false if the choice is incorrect"""
+        self.__choices[index].mode = index
+
+    def searchChoice(self, txt):
+
+        index = -1
+
+        for ch in self.__choices:
+            index += 1
+            if choice.txtEq(txt, ch.title):
+                return index
+        return -1
+
+
 
 
 
