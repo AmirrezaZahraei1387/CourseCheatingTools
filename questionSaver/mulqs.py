2704648499e58ee1ae0aa14d547b2c651fe3456d
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-08-10 14:42:52 -0600

    a new function for searching among questions is added to the MultipleQuestionS class.

--- questionSaver/mulqs.py
+++ questionSaver/mulqs.py
@@ -1,7 +1,9 @@
+from questionSaver.choice import txtEq
 
 
 class MultipleQuestionS:
     __questions: list = []
+    __name: str
 
     def __init__(self, questions: list):
         self.__questions = questions
@@ -14,3 +16,31 @@ class MultipleQuestionS:
 
     def append(self, value):
         self.__questions.append(value)
+
+    def searchQuestion(self, value: str, mode: str):
+        """value is something you which to search, and mode is the
+        mode for searching it could be EXACT or SIMILAR.
+        EXACT means to do this using operator ==.
+        and SIMILAR means to search using text similarities."""
+        index = -1
+
+        if mode.lower() == "exact":
+            for q in self.__questions:
+                index += 1
+                if q == value:
+                    return index
+            return -1
+
+        elif mode.lower() == "similar":
+            results = []
+            for q in self.__questions:
+                index += 1
+                if txtEq(value, q.name):
+                    results.append(index)
+            return results
+
+        else:
+            raise ValueError("unrecognized mode.")
+
+
+
