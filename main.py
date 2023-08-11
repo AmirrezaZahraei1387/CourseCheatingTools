44acc3cc91d3242d02d400603a4df2edfc6a54f8
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-08-11 11:07:27 -0600

    the main.py for the main part of program is added

---
+++ main.py
@@ -0,0 +1,30 @@
+import questionSaver as qs
+
+"""commands: 
+"""
+
+
+while True:
+
+    command = input("enter what you want to do?").lower()
+
+    if command == "add-question":
+        pass
+
+    elif command == "set-choice-mode":
+        pass
+
+    elif command == "add-choice":
+        pass
+
+    elif command == "remove-question":
+        pass
+
+    elif command == "get-answer":
+        pass
+
+    elif command == "exit":
+        pass
+
+    else:
+        break
